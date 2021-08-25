007c0d0231db64d6327a032f7e6a398277af97a7
sticks1988 <mikepb6691@aol.com>
2021-08-25 13:09:04 -0400

    adding questions to JS
    Adding questions to JS

--- Develope/javascript.js
+++ Develope/javascript.js
@@ -1,1 +1,54 @@
-zvzxvzxv
+var questionsEl = document.getElementById("questions");
+
+var quizQuestions = [{
+    question: "What language styles a page for HTML?",
+    choiceA: "Javascript",
+    choiceB: "CSS",
+    choiceC: "HTML Syntax",
+    choiceD: "Cool Whip",
+    correctAnswer: "b"},
+  {
+    question: "Where can we see errors in our code when on a website?",
+    choiceA: "While inspecting page",
+    choiceB: "By checking our code off the page",
+    choiceC: "Phoning a friend",
+    choiceD: "In safe mode",
+    correctAnswer: "a"},
+   {
+    question: "What is used to create functions?",
+    choiceA: "JavaScript",
+    choiceB: "CSS",
+    choiceC: "HTML",
+    choiceD: "Bootstrap",
+    correctAnswer: "a"},
+    {
+    question: "What is it called to change elements in a DOM tree?",
+    choiceA: "maniputlation",
+    choiceB: "walking",
+    choiceC: "running",
+    choiceD: "sprinting",
+    correctAnswer: "a"},
+    {
+    question: "How to tell if your CSS code is incorrect?",
+    choiceA: "The page simply will not display correctly",
+    choiceB: "It will always tell you why it isn't working",
+    choiceC: "By reloading the page over and over until it works",
+    choiceD: "You can't",
+    correctAnswer: "a"},  
+    {
+    question: "What does WWW stand for?",
+    choiceA: "World When Winter",
+    choiceB: "When We're Wet",
+    choiceC: "Well We Will",
+    choiceD: "World Wide Web",
+    correctAnswer: "d"},
+    {
+    question: "What HTML attribute references an external JavaScript file?",
+    choiceA: "class",
+    choiceB: "src",
+    choiceC: "href",
+    choiceD: "index",
+    correctAnswer: "b"},
+        
+    
+    ];
