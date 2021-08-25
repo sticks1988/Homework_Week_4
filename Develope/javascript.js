50ee3bdfef364f4167e8f04eaec3bdedab9e0565
sticks1988 <mikepb6691@aol.com>
2021-08-25 13:35:06 -0400

    functions
    added stating quiz and generating quiz functions as well functionality with the option selections in the button outputs
    
    added a timer to interval with the start quiz function
    
    added function to show high score and parsed the function to get scores, trimmed the value of the score
    
    implemented a replay function as well as clear score function to clear board after inputting initials
    
    added a answer checker function to alert when a user is correct or incorrect as well as removing time from the timer for an incorrect answer
    
    initialized the start button for the quiz
    
    double checked all global variables align with correct function syntax for JS

--- Develope/javascript.js
+++ Develope/javascript.js
@@ -77,3 +77,123 @@ var timeLeft = 60;
 var timerInterval;
 var score = 0;
 var correct;
+
+function generateQuizQuestion(){
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+function startQuiz(){
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time left: " + timeLeft;
+    
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1000);
+    testSection.style.display = "block";
+}
+function showScore(){
+    testSection.style.display = "none"
+    doneDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    scoreInputName.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+submitScoreBtn.addEventListener("click", function highscore(){
+
+    if(scoreInputName.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    }else{
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = scoreInputName.value.trim();
+        var currentHighscore = {
+            name : currentUser,
+            score : score
+        };
+    
+        scoreContainer.style.display = "flex";
+        scoreDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+        
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+
+    }
+    
+});
+
+function generateHighscores(){
+    scoreDisplayName.innerHTML = "";
+    scoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        scoreDisplayName.appendChild(newNameSpan);
+        scoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+function showHighscore(){
+    scoreContainer.style.display = "flex";
+    scoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+function clearScore(){
+    window.localStorage.clear();
+    scoreDisplayName.textContent = "";
+    scoreDisplayScore.textContent = "";
+}
+
+function replayQuiz(){
+    testSection.style.display = "none"
+    doneDiv.style.display = "none";
+    scoreContainer.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 60;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+// Answer Checker 
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        timeLeft -=5;
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+startQuizButton.addEventListener("click", startQuiz);
